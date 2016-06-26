1da9cd32c6831059b08b5fffaba0ed9e36ee6dca
Josh Zoffranieri <joshzoff@gmail.com>
2016-06-26 18:10:37 -0400

    Combine data structures in index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import BST from './src/binary-search-tree';
+import LinkedList from './src/linked-list';
+import List from './src/list';
+import Stack from './src/stack';
+
+export default {
+  BST,
+  LinkedList,
+  List,
+  Stack
+};
